1944ac8397951ecd376e8c1af79314400b48988e
F3rNaNDEZ57 <98757988+F3rNaNDEZ57@users.noreply.github.com>
2023-10-20 02:14:34 +0530

    subscriptionhub theory done

--- SubscriptionHubScreen.js
+++ SubscriptionHubScreen.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 
-export default function SubscriptionHubScreen() {
+export default function SubscriptionHubScreen({ navigation }) {
   const [sensors, setSensors] = useState([]);
-  
+  const [selectedSensors, setSelectedSensors] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/sensors');
+        const response = await axios.get('https://tadhack-readdb.onrender.com/sensors');
         setSensors(response.data);
       } catch (error) {
         console.error("Error fetching sensor data:", error);
@@ -17,20 +18,38 @@ export default function SubscriptionHubScreen() {
     fetchData();
   }, []);
 
+  const handleSensorSelect = (sensor) => {
+    setSelectedSensors(prev => {
+      if (prev.includes(sensor.sensor_id)) {
+        return prev.filter(id => id !== sensor.sensor_id);
+      } else {
+        return [...prev, sensor.sensor_id];
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Subscription Hub</Text>
       <FlatList
         data={sensors}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item) => item.sensor_id}
         renderItem={({ item }) => (
-          <View style={styles.sensorItem}>
-            <Text>Name: {item.name}</Text>
-            <Text>Type: {item.type}</Text>
+          <TouchableOpacity 
+            style={[
+              styles.sensorItem,
+              selectedSensors.includes(item.sensor_id) && styles.selectedSensorItem
+            ]}
+            onPress={() => handleSensorSelect(item)}
+            activeOpacity={0.6}
+          >
+            <Text>Name: {item.sensor_id}</Text>
+            <Text>Type: {item.sensor_type}</Text>
             <Text>Location: {item.location}</Text>
-          </View>
+          </TouchableOpacity>
         )}
       />
+      <Button title="SUBSCRIBE" onPress={() => navigation.navigate('Dashboard')} />
     </View>
   );
 }
@@ -39,6 +58,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 10,
+    paddingBottom: 20,
   },
   title: {
     fontSize: 24,
@@ -48,5 +68,13 @@ const styles = StyleSheet.create({
     padding: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+    backgroundColor: '#f5f5f5',
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  selectedSensorItem: {
+    backgroundColor: '#a3c9a8', // A green color to indicate selection, you can adjust this
+    borderColor: '#389638', // Darker green border for visibility
+    borderWidth: 1,
   },
 });
